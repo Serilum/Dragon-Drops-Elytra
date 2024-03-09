424d1c5d4c204e549f68725c38ab2f05a0d6ab4f
Rick South <rick@natamus.com>
2024-03-09 13:56:19 +0100

    (v3.3) Updated the mod to use the Collective's improved MessageFunctions instead of StringFunctions.

--- Common/src/main/java/com/natamus/dragondropselytra/util/Reference.java
+++ Common/src/main/java/com/natamus/dragondropselytra/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.dragondropselytra.util;
 public class Reference {
 	public static final String MOD_ID = "dragondropselytra";
 	public static final String NAME = "Dragon Drops Elytra";
-	public static final String VERSION = "3.2";
+	public static final String VERSION = "3.3";
 	public static final String ACCEPTED_VERSIONS = "[1.19.2]";
 }
