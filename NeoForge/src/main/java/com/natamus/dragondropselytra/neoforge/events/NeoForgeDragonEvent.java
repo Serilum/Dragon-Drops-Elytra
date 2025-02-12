42da7742d01762a89cbbc7643a8a71cb0d4612bf
Rick South <rick@natamus.com>
2025-02-12 23:37:02 +0100

    (v3.5) (Forge & Neo) Removed redundant event register annotations. [+1]

--- NeoForge/src/main/java/com/natamus/dragondropselytra/neoforge/events/NeoForgeDragonEvent.java
+++ NeoForge/src/main/java/com/natamus/dragondropselytra/neoforge/events/NeoForgeDragonEvent.java
@@ -4,9 +4,7 @@ import com.natamus.dragondropselytra.events.DragonEvent;
 import net.minecraft.world.entity.Entity;
 import net.neoforged.neoforge.event.entity.living.LivingDropsEvent;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
 
-@EventBusSubscriber
 public class NeoForgeDragonEvent {
 	@SubscribeEvent
 	public static void mobItemDrop(LivingDropsEvent e) {
