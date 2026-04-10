75b491ce48e3b7f6029692fac936a57126b8b529
Rick South <rick@natamus.com>
2026-04-10 02:57:40 +0200

    Dragon Drops Elytra source code for 26.1.2.

---
+++ NeoForge/src/main/java/com/natamus/dragondropselytra/neoforge/events/NeoForgeDragonEvent.java
@@ -0,0 +1,14 @@
+package com.natamus.dragondropselytra.neoforge.events;
+
+import com.natamus.dragondropselytra.events.DragonEvent;
+import net.minecraft.world.entity.Entity;
+import net.neoforged.neoforge.event.entity.living.LivingDropsEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+
+public class NeoForgeDragonEvent {
+	@SubscribeEvent
+	public static void mobItemDrop(LivingDropsEvent e) {
+		Entity entity = e.getEntity();
+		DragonEvent.mobItemDrop(entity.level(), entity, e.getSource());
+	}
+}
