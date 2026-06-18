0e1547f8e9b3b26afdd2d4f6e9031176ca8c76b4
Rick South <rick@natamus.com>
2026-06-18 23:59:31 +0200

    Updated Dragon Drops Elytra to Minecraft version 26.2.0.

--- Common/src/main/java/com/natamus/dragondropselytra/util/Reference.java
+++ Common/src/main/java/com/natamus/dragondropselytra/util/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
 	public static final String MOD_ID = "dragondropselytra";
 	public static final String NAME = "Dragon Drops Elytra";
 	public static final String VERSION = "3.6";
-	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
+	public static final String ACCEPTED_VERSIONS = "[26.2.0]";
 }
